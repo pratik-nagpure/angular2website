a93fe8b975536e6a1377353c794c9550cc2e5836
pratik-nagpure <pratiknagpure@hotmail.com>
2017-02-13 17:41:17 +0530

    Add files via upload

---
+++ app/main.ts
@@ -0,0 +1,7 @@
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+
+import {appRouterProviders} from './app.route';
+
+import {AppComponent} from './app.component';
+
+bootstrap(AppComponent,[appRouterProviders]);
